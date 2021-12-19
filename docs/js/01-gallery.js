193bb6f6c655ab0abba6b27fabc67e69b3233654
Serhii Pylypenko <jpee.dev@gmail.com>
2021-12-19 13:34:05 +0200

    develop feat(gallery) implement gallery v1 - task 01

--- docs/js/01-gallery.js
+++ docs/js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const makeImage = (data = {}) => (`
+  <div class="gallery__item">
+    <a class="gallery__link" href="${data.original}">
+      <img
+        class="gallery__image"
+        src="${data.preview}"
+        data-source="${data.original}"
+        alt="${data.description}"
+      />
+    </a>
+  </div>
+`);
+
+const closeModalHandler = (instance, evt) => {
+  if (evt.key === 'Escape') {
+    instance.close();
+    window.removeEventListener('keyup', closeModalHandler);
+  }
+};
+
+const openImageModal = (url, alt) => {
+  const instance = basicLightbox.create(`<img src="${url}" alt="${alt}">`);
+  instance.show();
+
+  if (instance.visible()) {
+    window.addEventListener('keyup', (evt) => closeModalHandler(instance, evt));
+  }
+};
+
+const onImagesClickHandler = (evt) => {
+  evt.preventDefault();
+  if (evt.target.classList.contains('gallery__image')) {
+    const fullImageUrl = evt.target.dataset.source;
+    const alt = evt.target.getAttribute('alt');
+    openImageModal(fullImageUrl, alt);
+  }
+};
+
+const app = () => {
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = galleryItems.map(makeImage).join('\n');
+  gallery.addEventListener('click', onImagesClickHandler);
+};
+
+app();
