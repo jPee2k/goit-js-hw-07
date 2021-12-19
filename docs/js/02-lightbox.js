46d0fa468e0c0e2f4395416c34cd708221376cb0
Serhii Pylypenko <jpee.dev@gmail.com>
2021-12-19 16:24:33 +0200

    develop feat(gallery) implement gallery v2

--- docs/js/02-lightbox.js
+++ docs/js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const makeImage = (data = {}) => (`
+  <a class="gallery__item" href="${data.original}">
+    <img class="gallery__image" src="${data.preview}" title="${data.description}" alt="${data.description}" />
+  </a>
+`);
+
+const app = () => {
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = galleryItems.map(makeImage).join('\n');
+
+  new SimpleLightbox('.gallery a', { disableRightClick: true, captionDelay: 250 });
+};
+
+app();
