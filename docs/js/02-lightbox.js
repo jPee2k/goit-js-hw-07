799b62f64841cca0179300534c50eb17786fab8e
Serhii Pylypenko <jpee.dev@gmail.com>
2021-12-20 11:43:55 +0200

    develop fix(lightbox gallery) use destructuring

--- docs/js/02-lightbox.js
+++ docs/js/02-lightbox.js
@@ -1,9 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const makeImage = (data = {}) => (`
-  <a class="gallery__item" href="${data.original}">
-    <img class="gallery__image" src="${data.preview}" title="${data.description}" alt="${data.description}" />
+const makeImage = ({ original, preview, description } = {}) => (`
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" title="${description}" alt="${description}" />
   </a>
 `);
 
